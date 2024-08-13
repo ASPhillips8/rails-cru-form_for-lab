50b46fb438c10254c3b7a6ffb2a315c1d9b2e132
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-13 11:12:49 -0400

    Add index route for songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :artists, only: [:new, :create, :edit, :show, :update]
   resources :genres, only: [:new, :create, :edit, :show, :update]
-  resources :songs, only: [:new, :create, :edit, :show, :update]
+  resources :songs, only: [:index, :new, :create, :edit, :show, :update]
 end
