015add534c32e526206d8ead30e6bc0c270186aa
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-13 09:05:21 -0400

    Fix songs table

--- db/schema.rb
+++ db/schema.rb
@@ -21,18 +21,18 @@ ActiveRecord::Schema.define(version: 20240813125244) do
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
-    t.integer  "artist_id"
-    t.integer  "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["artist_id"], name: "index_genres_on_artist_id"
-    t.index ["genre_id"], name: "index_genres_on_genre_id"
   end
 
   create_table "songs", force: :cascade do |t|
     t.string   "name"
+    t.integer  "artist_id"
+    t.integer  "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+    t.index ["genre_id"], name: "index_songs_on_genre_id"
   end
 
 end
